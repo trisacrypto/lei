c77108094e70f4411c53f0c4577b17aa4af17c34
Benjamin Bengfort <benjamin@bengfort.com>
2023-07-07 17:03:19 -0500

    Registration Authorities

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# lei
-A Go library for working with Legal Entity Identifiers (LEIs) as defined in ISO 17442-1:2020
+# Legal Entity Identifier Go Library
+
+A Go library for working with Legal Entity Identifiers (LEIs) as defined in [ISO 17442-1:2020](https://www.iso.org/standard/78829.html)
+
+This library is a port of the [leim](https://gitlab.com/21analytics/lei) Rust library developed by 21 Analytics.
+
+## Example
+
+```go
+import "github.com/trisacrypto/lei"
+
+func main() {
+    // Parse a valid LEI and check it
+    if entityID, err := lei.Parse("2594007XIACKNMUAW223"); err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println(entityID.Check())
+
+    // Parse an invalid LEI and print the invalid checksum error.
+    _, err := lei.Parse("2594007XIACKNMUAW222")
+    fmt.Println(err)
+}
+```
+
+## Usage
+
+Add the `lei` dependency to your `go.mod`:
+
+```
+$ go get github.com/trisacrypto/lei
+```
+
+
+## License
+
+This project is licensed under the MIT license to match the license of the upstream Rust library this package was ported from.
---
+++ authorities_test.go
@@ -0,0 +1,41 @@
+package lei_test
+
+import (
+	"errors"
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"github.com/trisacrypto/lei"
+)
+
+func TestRAIndex(t *testing.T) {
+	testCases := []struct {
+		input string
+		err   error
+	}{
+		{"RA000001", nil},
+		{"RA888888", nil},
+		{"RA999999", nil},
+		{"RA100001", errors.New("unknown registration authority: RA100001")},
+		{"AA000001", errors.New("unknown registration authority: AA000001")},
+		{"RA00009", errors.New("unknown registration authority: RA00009")},
+		{"RA0000945", errors.New("unknown registration authority: RA0000945")},
+		{"RA100094", errors.New("unknown registration authority: RA100094")},
+	}
+
+	for i, tc := range testCases {
+		ra, err := lei.NewRA(tc.input)
+		if tc.err != nil {
+			require.EqualError(t, err, tc.err.Error(), "test case %d expected an error", i)
+			require.Equal(t, "UNKNOWN", ra.String(), "test case %d expected invalid RA", i)
+		} else {
+			require.Equal(t, tc.input, ra.String(), "test case %d expected a valid RA")
+		}
+	}
+}
+
+func TestRegistrationAuthoritiesAreSorted(t *testing.T) {
+	authorities := lei.RegistrationAuthorities()
+	require.True(t, sort.StringsAreSorted(authorities), "the registration authories must be sorted")
+}
